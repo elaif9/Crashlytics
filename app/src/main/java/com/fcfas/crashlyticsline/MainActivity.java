86408a00f212e7c43e36d7e58049485b9b42cdf3
Alif Nur Rachman <alifnurrachman907@gmail.com>
2019-04-25 08:18:41 +0700

    C15 - Send Crash when button clicked

--- app/src/main/java/com/fcfas/crashlyticsline/MainActivity.java
+++ app/src/main/java/com/fcfas/crashlyticsline/MainActivity.java
@@ -2,12 +2,16 @@ package com.fcfas.crashlyticsline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.btn_force:
+                Crashlytics.log(Log.DEBUG, TAG, "Log some message before a crash happen");
+                Crashlytics.getInstance().crash();
+                break;
+            case R.id.btn_exception:
+				/*
+				try {
+					throw new NullPointerException();
+				} catch (NullPointerException ex) {
+					Crashlytics.logException(ex);
+					Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+				}
+				*/
+                Button btn = findViewById(R.id.btn_exception);
+                btn.setTextColor(null);
+                break;
+        }
     }
 }
