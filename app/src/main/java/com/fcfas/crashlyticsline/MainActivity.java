602c1948ea373018309c65571b5dc4750911e605
Alif Nur Rachman <alifnurrachman907@gmail.com>
2019-04-25 08:08:40 +0700

    C12 - Implement View OnClickListener in class Main Activity

--- app/src/main/java/com/fcfas/crashlyticsline/MainActivity.java
+++ app/src/main/java/com/fcfas/crashlyticsline/MainActivity.java
@@ -2,15 +2,22 @@ package com.fcfas.crashlyticsline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
